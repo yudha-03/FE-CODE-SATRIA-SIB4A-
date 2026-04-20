1e2dc60f96ceb33e0b5476e801cacb20d8dd00d0
Satria <satriapradanaryudha@gmail.com>
2026-04-20 12:46:07 +0800

    tambah folder lean dan gambar

---
+++ app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function LearnPage(){
+    return(
+        <div>
+            <div>
+                <img src="/palette.svg" alt="Design" />
+                <p>Design</p>
+            </div>
+            <div>
+                <p>Figma</p>
+                <p>8 Hours</p>
+            </div>
+        </div>
+    )
+}

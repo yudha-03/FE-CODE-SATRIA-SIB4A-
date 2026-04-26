a3ef7e80f7716b8ec86d69c137136be933636bc7
yudha-03 <satriapradanaryudha@gmail.com>
2026-04-27 01:16:23 +0800

    tambah css

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,13 +1,14 @@
+import styles from "./page.module.css"
 export default function LearnPage(){
     return(
-        <div>
-            <div>
+        <div className={styles.main_container_1}>
+            <div className={styles.container_1}>
                 <img src="/palette.svg" alt="Design" />
-                <p>Design.</p>
+                <p>Design</p>
             </div>
-            <div>
-                <p>Figma.</p>
-                <p>8 Hours.</p>
+            <div className={styles.container_2}>
+                <p>Figma</p>
+                <p>8 Hours</p>
             </div>
         </div>
     )

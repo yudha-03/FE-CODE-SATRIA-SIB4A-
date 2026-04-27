f78b73051c0e8211e786d9595afc0e05ed677c3a
yudha-03 <satriapradanaryudha@gmail.com>
2026-04-27 12:15:55 +0800

    update css

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,15 +1,37 @@
-import styles from "./page.module.css"
+
 export default function LearnPage(){
     return(
-        <div className={styles.main_container_1}>
-            <div className={styles.container_1}>
-                <img src="/palette.svg" alt="Design" />
-                <p>Design</p>
+        <section className="h-screen bg-gray-100 flex gap-4">
+            <div className="bg-purple-300 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-blue-950">
+                <div className="flex gap-4" >
+                    <img src="palette.svg" alt="Design" />
+                    <p>Design</p>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold">Figma</h2>
+                    <p className="font-bold">8 Hours ago</p>
+                 </div>
             </div>
-            <div className={styles.container_2}>
-                <p>Figma</p>
-                <p>8 Hours</p>
+             <div className="bg-orange-300 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-blue-950">
+                <div className="flex gap-4" >
+                    <img src="palette.svg" alt="Design" />
+                    <p>Coding</p>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold">Python</h2>
+                    <p className="font-bold">2 days ago</p>
+                 </div>
             </div>
-        </div>
+             <div className="bg-red-300 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-blue-950">
+                <div className="flex gap-4" >
+                    <img src="palette.svg" alt="Design" />
+                    <p>Design</p>
+                </div>
+                <div>
+                    <h2 className="text-4xl font-bold">Sketch</h2>
+                    <p className="font-bold">4 days ago</p>
+                 </div>
+            </div>
+        </section>
     )
 }
